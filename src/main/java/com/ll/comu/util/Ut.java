44725bba00dff3fff216c2e8ccb313d59e8288e9
SangHun <shzero211@naver.com>
2022-07-28 14:00:01 +0900

    array+map_의 JSON_을 객체형태로 변환

--- src/main/java/com/ll/comu/util/Ut.java
+++ src/main/java/com/ll/comu/util/Ut.java
@@ -2,11 +2,13 @@ package com.ll.comu.util;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.comu.ArticleDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Ut {
     public static class json{
@@ -22,13 +24,27 @@ public class Ut {
             }
 
         }
-        public static <T> T toObject(String jsonStr,Class<T> cl,T defaultValue){
+        public static <T> T toObj(String jsonStr,Class<T> cl,T defaultValue){
         try{
             return (T)om.readValue(jsonStr, cl);
         }catch (JsonProcessingException e){
             return defaultValue;
 
         }
+        }
+        public static <T> List<T> toObj(String jsonStr, TypeReference<List<T>> typeReference, List<T> defaultValue) {
+            try {
+                return om.readValue(jsonStr, typeReference);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+        public static <K, V> Map<K, V> toMap(String jsonStr, TypeReference<Map<K, V>> typeReference, Map<K, V> defaultValue) {
+            try {
+                return om.readValue(jsonStr, typeReference);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
         }
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,4 +1,5 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.ll.comu.ArticleDto;
 import com.ll.comu.util.Ut;
 import org.junit.jupiter.api.Test;
@@ -52,10 +53,33 @@ public class AppTest {
                 """.trim());
     }
     @Test
-    void ObjectMapper_json_toObject(){
+    void ObjectMapper_json_toObj(){
         ArticleDto articleDtoOrigin=new ArticleDto(1,"제목","내용");
         String jsonStr=Ut.json.toStr(articleDtoOrigin,"");
-        ArticleDto articleDto=Ut.json.toObject(jsonStr,ArticleDto.class,null);
+        ArticleDto articleDto=Ut.json.toObj(jsonStr,ArticleDto.class,null);
         assertThat(articleDtoOrigin).isEqualTo(articleDto);
+    }
+    @Test
+    void ObjectMapper_json_toArticleDtoMap(){
+        Map<String,ArticleDto> mapOri=new HashMap<>();
+        mapOri.put("가장오래된",new ArticleDto(1,"제목1","내용1"));
+        mapOri.put("최신",new ArticleDto(2,"제목2","내용2"));
+        String jsonStr=Ut.json.toStr(mapOri,"");
+        Map<String,ArticleDto> map=Ut.json.toMap(jsonStr, new TypeReference<Map<String, ArticleDto>>() {
+        },null);
+        System.out.println(map.get("최신"));
+    }
+    @Test
+    void ObjectMapper__jsonStrToArticleDtoList() {
+        List<ArticleDto> articleDtos = new ArrayList<>();
+        articleDtos.add(new ArticleDto(1, "제목1", "내용1"));
+        articleDtos.add(new ArticleDto(2, "제목2", "내용2"));
+
+        String jsonStr = Ut.json.toStr(articleDtos, "");
+
+        List<ArticleDto> articleDtosFromJson = Ut.json.toObj(jsonStr, new TypeReference<>() {
+        }, null);
+
+        assertThat(articleDtosFromJson).isEqualTo(articleDtos);
     }
 }
