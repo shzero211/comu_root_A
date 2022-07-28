dff7a7e0b8451668ce110893fd24be7af3bf3e7c
SangHun <shzero211@naver.com>
2022-07-28 10:24:26 +0900

    Jackson_추가및 json 생성

---
+++ src/main/java/com/ll/comu/util/Ut.java
@@ -0,0 +1,19 @@
+package com.ll.comu.util;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Ut {
+    public static class json{
+        public static String toJsonStr(Object obj,String defaultValue){
+            ObjectMapper om=new ObjectMapper();
+            try{
+                return om.writeValueAsString(obj);
+            }catch (JsonProcessingException e){
+                return defaultValue;
+            }
+
+        }
+    }
+}
